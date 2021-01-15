59a3cd837166162efb0ec682105576457daa2d09
jonnyMcBourne <jonathan.hernandez.oliva@hotmail.com>
2021-01-15 12:40:19 -0600

    big fix 3

--- src/auth/authReducer.js
+++ src/auth/authReducer.js
@@ -1,22 +1,15 @@
 import { typesLogin } from "../types/types";
-const {LOGIN,LOGOUT}= typesLogin
+const { LOGIN, LOGOUT } = typesLogin;
 
-export const authReducer = (state={}, action) => {
-    
-    switch (action.type) {
-        case LOGIN:
-           return {user:action.payload,logged:true}
+export const authReducer = (state = {}, action) => {
+  switch (action.type) {
+    case LOGIN:
+      return { user: action.payload, logged: true };
 
-            break;
-        case LOGOUT:
-            return {user:"",logged:false}
-            break;
-    
-        default:
-            return state;
-            break;
-    }
-    
-    
-    return {}
-}
+    case LOGOUT:
+      return { user: "", logged: false };
+
+    default:
+      return state;
+  }
+};
